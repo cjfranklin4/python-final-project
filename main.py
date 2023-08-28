cedb95e4e7d8fbf529679741065d8b955e0f9d38
Cayla Franklin <cjfranklin4@gmail.com>
2023-08-28 20:13:38 +0000

    update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import redirect
 from flask import url_for
 from flask import render_template
 from flask import request
-from sqlite3 import sqlite
+import sqlite3 as sql
 
 app = Flask(__name__)
 
@@ -14,16 +14,48 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
-@app.route("/shop")
-def shopping():
-    return render_template("shop.html")
-with sql.connect(pokemon.db) as con():
+#Get items from the database
+def get_pokemon():
+    con = sql.connect("pokemon.db")
+    con.row_factory = sql.Row
+    
     cur = con.cursor()
-    cur.execute("SELECT * FROM POKEMON")
+    cur.execute("SELECT * from POKEMON")     
+    
+    pokemon = cur.fetchall() 
+    #print("pokemon", pokemon)
+    con.close()
+    return pokemon
 
-with sql.connect(berries.db) as con2():
-    cur = con2.cursor()
-    cur.execute("SELECT * FROM BERRIES")
+def get_berries():
+    con = sql.connect("berries.db")
+    con.row_factory = sql.Row
+    
+    cur = con.cursor()
+    cur.execute("SELECT * from BERRIES")     
+    
+    berries = cur.fetchall() 
+    #print("berries", berries)
+    con.close()
+    return berries
+
+@app.route('/shop')
+def shopping():
+
+    #GET REQUEST
+    poke_array = get_pokemon()
+    berries = get_berries()
+
+
+    #NEED POST REQUEST SECTION
+    """
+        POST request when you click the "Add to cart button"
+        MAX 3 items in cart, if more than 3 items, you get an alert/warning about cart being full
+
+        a new database (cart.db) should be created that keeps track of the items being added or removed from your cart
+    """
+
+    return render_template('shop.html', poke_array=poke_array, berries=berries)
 
 
 
@@ -31,7 +63,6 @@ with sql.connect(berries.db) as con2():
 def checkout():
     return render_template("checkout.html")
 
-con.close()
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=2224, debug=True)
