09f75dc41c81f285d7eb290add6bd8df75d38489
Nallen917 <norward1992@gmail.com>
2023-08-29 19:20:10 +0000

    pytests are complete

--- testing.py
+++ testing.py
@@ -2,8 +2,7 @@ import sqlite3 as sql
 import pytest
 from main import get_pokemon
 from main import get_berries
-
-
+from main import app, remove_from_cart, get_cart
 def test_get_pokemon():
     # Setup
     # You might need to create a temporary database with test data for this test
@@ -22,8 +21,25 @@ def test_get_berries():
     assert isinstance(result, list)
     assert len(result) > 0
 
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
 
+    yield client
 
+def test_remove_from_cart(client):
+    with client:
+        response = client.post('/add-to-cart', data={
+            'item_id': 1,
+            'item_type': 'pokemon',
+            'item_name': 'Pikachu'
+        })
+        assert response.status_code == 302
 
-# If you want to run the tests directly when the script is executed
+    with client:
+        response = client.post('/remove_from_cart/1')
+        assert response.status_code == 302  
 
+    cart = get_cart()
+    assert 1 not in [item['ID'] for item in cart]
