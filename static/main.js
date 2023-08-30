38d0d7cec486984c016abe69f01819350132e94d
Cayla Franklin <cjfranklin4@gmail.com>
2023-08-30 08:26:31 -0500

    updated styling more

--- static/main.js
+++ static/main.js
@@ -1,6 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleCartButton = document.getElementById('toggleCartButton');
+    const cartContainer = document.querySelector('#shopping-cart');
+
+    toggleCartButton.addEventListener('click', function () {
+        console.log("I work")
+        cartContainer.classList.toggle('active');
+    });
+});
+
 function full_cart() {
     var cartSize = document.querySelectorAll('.cart-item.shop-page').length;
-    console.log("I WORK", cartSize)
+    //console.log("I WORK", cartSize)
     if (cartSize >= 3) {
         alert('Cart is full! Please remove items before adding more.');
     }
